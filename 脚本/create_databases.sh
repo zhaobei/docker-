7eac9fc445c943eaf75f8f50663e94eaca62060c
zhaobei <1157037674@qq.com>
2020-10-04 16:38:23 +0800

    整合save和load
    Signed-off-by: zhaobei <1157037674@qq.com>

---
+++ 脚本/create_databases.sh
@@ -0,0 +1,7 @@
+mysql \
+  -h 106.13.66.207 \
+  -P 3306 \
+  -u root \
+  -p \
+  -e "CREATE DATABASE dailyfresh default character set utf8mb4 collate utf8mb4_unicode_ci;"
+#CREATE DATABASE IF NOT EXISTS yourdbname DEFAULT CHARSET utf8 COLLATE utf8_general_ci;
