b661cb8694f2fb47886b571f81151a4bcee37a2f
zhaobei <1157037674@qq.com>
2020-10-04 16:31:24 +0800

    整合save和load
    Signed-off-by: zhaobei <1157037674@qq.com>

--- save_images.py
+++ save_images.py
@@ -1,25 +1,47 @@
-import re
 import os
+import re
 from subprocess import PIPE, Popen, STDOUT
-import time
 
-if __name__ == "__main__":
+
+def save_docker():
+    save_file = str(input("请输入保存镜像地址的绝对路径>>>>>>"))
+    os.system('mkdir -p %s' % save_file)
     p = Popen('docker images', shell=True, stdout=PIPE, stderr=STDOUT)
     for line in p.stdout.readlines():
         string = str(line)
-        m = re.match(r'(^ufleet[^\s]*\s*)\s([^\s]*\s)', string)
+        m = re.match(r'(^[^\s]*\s*)\s([^\s]*\s)', string)
         image_name = m.group(1).strip(' ')
-        # tag
         itag = m.group(2)
-        # tar包的名字
         tar_name = image_name.split('/')[-1]
-        print(tar_name)
         tarball = tar_name + '.tar'
         ifull = image_name + ':' + itag
-        # save
+
         cmd = 'docker save -o ' + tarball + ' ' + ifull
         print(os.system(cmd))
-        os.system('mkdir -p /data/python_save_images/')
-        # 将tar包放在临时目录
-        print(os.system('mv %s /data/python_save_images/' % tarball))
+
+        os.system('mv %s %s' % (tarball, save_file))
         retval = p.wait()
+
+
+def load_images():
+    tarball = str(input("请输入load镜像地址的绝对路径>>>>>>"))
+    os.chdir(tarball)
+    files = os.listdir(tarball)
+    for filename in files:
+        print(filename)
+        os.system('docker load -i %s' % filename)
+
+
+if __name__ == '__main__':
+    i = 0
+    while i < 3:
+        print("保存当前harbor中的全部镜像请摇1")
+        print("load 某一文件中的全部镜像请摇2")
+        worker = int(input(">>>>>>>"))
+        if worker == 1:
+            save_docker()
+        elif worker == 2:
+            load_images()
+        else:
+            print("输入有误请重新输入，如果停止可使用control + c 停止程序")
+            i += 1
